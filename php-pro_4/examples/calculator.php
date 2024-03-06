51207231b78b85c2859e41685587ad93e4c9b407
николай шопин <nikshopin@gmail.com>
2024-03-06 23:30:28 +0200

    calculator task completed

--- php-pro_4/examples/calculator.php
+++ php-pro_4/examples/calculator.php
@@ -4,15 +4,15 @@ interface ICalculator
 {
     public function __construct(array $arrNumbers);
 
-    public function doWithNumbers():void;
+    public function actionWithNumbers():void;
 }
 
 abstract class ACalculator implements ICalculator{
     public function __construct(protected array $arrNumbers){
         ArrNumbersValidator::arrNumbersValid($this->arrNumbers);
-        $this->doWithNumbers();
+        $this->actionWithNumbers();
     }
-    public function doWithNumbers(): void
+    public function actionWithNumbers(): void
     {
         // TODO: Implement doWithNumbers() method.
     }
@@ -22,33 +22,37 @@ abstract class ACalculator implements ICalculator{
 
 class SumNumber extends ACalculator
 {
-    public function doWithNumbers():void{
+    public function actionWithNumbers():void{
       echo 'Сумма чисел = ' . array_sum($this->arrNumbers) .  PHP_EOL;
     }
 }
 
-//class SubtractingNumber extends ACalculator
-//{
-//    public function doWithNumbers():void{
-//        $res= $this->arrNumbers[0];
-//        for ($i=1; $i < array_count_values($this->arrNumbers); $i++){
-//            $res -= $this->arrNumbers[$i];
-//        }
-//       echo 'Вычитание чисел = ' . $res . PHP_EOL;
-//    }
-//}
-//
+class SubtractingNumber extends ACalculator
+{
+    public function actionWithNumbers():void{
+        $res= null;
+        foreach ($this->arrNumbers as $value){
+            $res == null ? $res = $value : $res -=$value;
+        }
+       echo 'Вычитание чисел = ' . $res . PHP_EOL;
+    }
+}
+
 class DividingNumber extends ACalculator
 {
-    public function doWithNumbers():void{
+    public function actionWithNumbers():void{
         ArrNumbersValidator::valueInArrIsNull($this->arrNumbers);
-        foreach ()
+        $res =null;
+        foreach ($this->arrNumbers as $value){
+            $res === null ? $res = $value : $res/=$value;
+        }
+        echo 'Деление чисел = ' . $res . PHP_EOL;
     }
 }
 
 class MultiplyingNumber extends ACalculator
 {
-    public function doWithNumbers():void{
+    public function actionWithNumbers():void{
         echo 'Произведение чисел = ' . array_product($this->arrNumbers)  . PHP_EOL;
     }
 }
@@ -56,7 +60,6 @@ class MultiplyingNumber extends ACalculator
 class ArrNumbersValidator
 {
     static function arrNumbersValid(array $arrNumbers): void{
-
         foreach ($arrNumbers as $arrNumber){
             if (!(is_int($arrNumber) or is_float($arrNumber))){
                 throw new InvalidArgumentException('Value ' . $arrNumber . ' is not number');
@@ -70,10 +73,10 @@ class ArrNumbersValidator
             }
         }
     }
-
-
 }
-$sumNumbers = new SumNumber([40, 50, 77.9, 55]);
-//$SubtractingNumber = new SubtractingNumber([100,10]);
 
-$MultiplyingNumbers = new MultiplyingNumber([10,5]);
+
+$sumNumbers = new SumNumber([40, 50, 77.9, 55]);
+$divNumber = new DividingNumber([60, 20, 6]);
+$SubtractingNumber = new SubtractingNumber([100, 10]);
+$MultiplyingNumbers = new MultiplyingNumber([10, 0]);
